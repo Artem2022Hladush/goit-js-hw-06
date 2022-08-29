e070115744b9696997aef66658bb002329862bcd
Артем Гладуш <m1hommage5008@gmail.com>
2022-08-29 20:49:14 +0300

    add task 5

--- js/task-05.js
+++ js/task-05.js
@@ -4,3 +4,13 @@ const refs = {
 };
 
 
+refs.input.addEventListener('input', onInputChange );
+
+
+function onInputChange(event) {
+	if (event.currentTarget.value.length === 0) {
+		return refs.label.textContent = 'Anonymous'
+	}
+	
+	refs.label.textContent = event.currentTarget.value;
+} 
