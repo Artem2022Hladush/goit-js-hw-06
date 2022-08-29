791124167d7cefab854b8364c9648cdd066daa83
Артем Гладуш <m1hommage5008@gmail.com>
2022-08-29 21:15:17 +0300

    add task 6

---
+++ js/task-06.js
@@ -0,0 +1,13 @@
+const input = document.querySelector('#validation-input');
+
+
+const onInputBlur = event => {
+	if(event.currentTarget.value.length === Number(input.dataset.length)) {
+	return	input.classList.add('valid')
+	}
+return input.classList.replace('valid', 'invalid')
+
+}
+
+
+console.log(input.addEventListener('blur', onInputBlur))
