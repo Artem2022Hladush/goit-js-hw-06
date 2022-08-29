e39cd3a058da7acb725cc953e06896648a33ee2b
Артем Гладуш <m1hommage5008@gmail.com>
2022-08-29 22:58:03 +0300

    add task 8

--- js/task-07.js
+++ js/task-07.js
@@ -1,7 +1,7 @@
 const range = document.querySelector("#font-size-control")
 const span = document.querySelector("#text");
 
-
+console.dir(span)
 function fonSize () {
 const value = range.value + 'px'
 span.style.fontSize = value;
--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,28 @@
+const form = document.querySelector('.login-form');
 
+
+const onFormsubmit = event => {
+	event.preventDefault();
+	
+const formElements = event.currentTarget.elements;
+
+const email = formElements.email.value;
+const password = formElements.password.value;
+
+const formData = {
+	email,
+	password,
+};
+
+
+if (email === '' || password === '') {
+	return alert('All fields must be filled in.')
+};
+
+console.log(formData);
+
+event.currentTarget.reset();
+};
+
+
+form.addEventListener('submit', onFormsubmit);
