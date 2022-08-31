392656faf8fd56c35d1973ec203974aee24d47ad
Артем Гладуш <m1hommage5008@gmail.com>
2022-08-31 19:24:42 +0300

    bax fixx task 6

--- js/task-06.js
+++ js/task-06.js
@@ -1,9 +1,9 @@
 const input = document.querySelector('#validation-input');
 
-
+input.classList.add('invalid')
 const onInputBlur = event => {
 	if(event.currentTarget.value.length === Number(input.dataset.length)) {
-	return	input.classList.add('valid');
+	return	input.classList.replace('invalid','valid');
 	}
 return input.classList.replace('valid', 'invalid');
 
