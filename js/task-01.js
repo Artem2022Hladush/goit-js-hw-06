2d37480b58354eb6167da763ed2eb5c8b4eaff0c
Артем Гладуш <m1hommage5008@gmail.com>
2022-08-31 19:12:39 +0300

    bag fixx task 1

--- js/task-01.js
+++ js/task-01.js
@@ -3,12 +3,14 @@ const categoriesEl = document.querySelector('#categories');
 console.dir(categoriesEl);
 console.log(`Number of categories: ${categoriesEl.children.length}`);
 
-console.log(`Category: ${categoriesEl.children[0].firstElementChild.textContent}`);
+const items = document.querySelectorAll('.item');
 
-console.log(`Elements: ${categoriesEl.children[0].childNodes[3].children.length}`);
+const liEl = document.querySelector('.item > ul');
+console.log(liEl)
 
-console.log(`Category: ${categoriesEl.children[1].firstElementChild.textContent}`);
-console.log(`Elements: ${categoriesEl.children[1].childNodes[3].children.length}`);
+const liElLength = liEl.children.length;
 
-console.log(`Category: ${categoriesEl.children[2].firstElementChild.textContent}`);
-console.log(`Elements: ${categoriesEl.children[2].childNodes[3].children.length}`);
+const titleAndULlength =items.forEach(item => {
+	console.log(`Category: ${item.textContent}`)
+	console.log(`Elements: ${liElLength}`)
+});
