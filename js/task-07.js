b7462b04d9fbd8c26beaac30b16fbce17ea70eae
Артем Гладуш <m1hommage5008@gmail.com>
2022-08-31 19:31:50 +0300

    bag fixx task 7

--- js/task-07.js
+++ js/task-07.js
@@ -1,10 +1,32 @@
-const range = document.querySelector("#font-size-control")
-const span = document.querySelector("#text");
+// const range = document.querySelector("#font-size-control")
+// const span = document.querySelector("#text");
 
-console.dir(span)
-function fonSize () {
-const value = range.value + 'px'
-span.style.fontSize = value;
+// console.dir(span)
+// function fonSize () {
+// const value = range.value + 'px'
+// span.style.fontSize = value;
+// }
+
+// range.addEventListener('input', fonSize)
+
+
+const input = document.querySelector(`#font-size-control`);
+const span = document.querySelector(`#text`);
+
+const onDraggingSlider = event => {
+	const evt = event.currentTarget;
+
+	if (evt.value >= 40 && evt.value < 60) {
+		return span.style.fontSize = `20px`
+
+		} else if (evt.value >= 60 && evt.value < 96) {
+			return span.style.fontSize = `25px`
+
+		} else if (evt.value >= 96) {
+			return span.style.fontSize = `30px`
+		}
+
+			return span.style.fontSize = `15px`
 }
 
-range.addEventListener('change', fonSize)
+input.addEventListener(`input`, onDraggingSlider);
