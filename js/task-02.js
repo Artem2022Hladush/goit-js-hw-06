83aa92c7112b35506f7938ce0aab829a132ef2c9
Артем Гладуш <m1hommage5008@gmail.com>
2022-08-31 19:18:11 +0300

    bax fixx task 2

--- js/task-02.js
+++ js/task-02.js
@@ -7,47 +7,17 @@ const ingredients = [
   'Condiments',
 ];
 
+
+
+const liMarkup = ingredients.map(element => {
+  const li = document.createElement('li');
+  li.classList.add('item');
+  li.textContent = element;
+  return li;
+})
+
+
 const ingredientsEl = document.querySelector('#ingredients');
 
-const potatoEl = document.createElement( "li" );
-potatoEl.textContent = ingredients[0];
-potatoEl.classList.add('ingridients__item');
-
-console.log(potatoEl);
-
-const mushroomEl = document.createElement( "li" );
-mushroomEl.textContent = ingredients[1];
-mushroomEl.classList.add('ingridients__item');
-
-console.log(mushroomEl);
-
-const garlicEl = document.createElement( "li" );
-garlicEl.textContent = ingredients[2];
-garlicEl.classList.add('ingridients__item');
-
-console.log(garlicEl);
-
-
-const tomatoEl = document.createElement( "li" );
-tomatoEl.textContent = ingredients[3];
-tomatoEl.classList.add('ingridients__item');
-
-console.log(tomatoEl);
-
-
-const herbEl = document.createElement( "li" );
-herbEl.textContent = ingredients[4];
-herbEl.classList.add('ingridients__item');
-
-console.log(herbEl);
-
-
-const condimentEl = document.createElement( "li" );
-condimentEl.textContent = ingredients[5];
-condimentEl.classList.add('ingridients__item');
-
-console.log(condimentEl);
-
-
-ingredientsEl.append(potatoEl, mushroomEl, garlicEl,  tomatoEl, herbEl, condimentEl )
+ingredientsEl.append(...liMarkup);
 
